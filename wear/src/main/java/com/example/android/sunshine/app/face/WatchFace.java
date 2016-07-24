a20ebbe2dfca631f976ba9ad8d2fef796816bc78
Chirag <me@chi6rag.net>
2016-07-24 15:03:45 +0530

    Refactoring rendering code

--- wear/src/main/java/com/example/android/sunshine/app/face/WatchFace.java
+++ wear/src/main/java/com/example/android/sunshine/app/face/WatchFace.java
@@ -13,7 +13,20 @@ import com.example.android.sunshine.app.timer.Time;
 import com.example.android.sunshine.app.timer.TimeViewModel;
 
 public class WatchFace {
-    final String DEGREE = "\u00b0";
+    public static final String TYPEFACE_SANS_SERIF_LIGHT = "sans-serif-light";
+    public static final int INDEX_TEXT_START = 0;
+    public static final int TEXT_SIZE_TIME = 56;
+    public static final int OFFSET_Y_TIME = 50;
+    public static final int OFFSET_Y_DATE = 5;
+    public static final int TEXT_SIZE_DATE = 24;
+    public static final int OFFSET_X_HIGH_TEMPERATURE = 5;
+    public static final int TEXT_SIZE_HIGH_TEMPERATURE = 40;
+    public static final int OFFSET_Y_HIGH_TEMPERATURE = 80;
+    public static final int OFFSET_X_LOW_TEMPERATURE = 70;
+    public static final int TEXT_SIZE_LOW_TEMPERATURE = 40;
+    public static final int OFFSET_Y_LOW_TEMPERATURE = 80;
+    public static final int OFFSET_X_WEATHER_ICON = 110;
+    public static final int OFFSET_Y_WEATHER_ICON = 30;
 
     private final Canvas canvas;
     private final Resources resources;
@@ -27,80 +40,28 @@ public class WatchFace {
 
     public void show(Time time, Double low, Double high, Bitmap bitmap, boolean isInAmbientMode) {
         TimeViewModel timeViewModel = new TimeViewModel(time);
+        WeatherViewModel weatherViewModel = new WeatherViewModel(low, high, bitmap);
         int height = canvas.getHeight();
         int width = canvas.getWidth();
-        float centerY = height / 2f;
-        float centerX = width / 2f;
-        drawWatchface(canvas, timeViewModel, centerY, centerX, isInAmbientMode, low, high, bitmap);
+        drawWatchface(canvas, timeViewModel, center(height), center(width), isInAmbientMode, weatherViewModel);
+    }
+
+    private float center(int height) {
+        return height / 2f;
     }
 
     private void drawWatchface(Canvas canvas, TimeViewModel timeViewModel, float centerY,
-                               float centerX, boolean isInAmbientMode, Double low,
-                               Double high, Bitmap weatherIcon) {
+                               float centerX, boolean isInAmbientMode,
+                               WeatherViewModel weatherViewModel) {
 
         setBackground(canvas, isInAmbientMode);
         String formattedTime = timeViewModel.formattedTime();
         String formattedDate = timeViewModel.formattedDate();
-        drawTime(canvas, formattedTime, centerX, centerY, bigTextPaint(56));
-        drawDateBelowTime(canvas, formattedDate, centerX, centerY, lightPaint(24), isInAmbientMode);
+        drawTime(canvas, formattedTime, centerX, centerY, isInAmbientMode);
+        drawDateBelowTime(canvas, formattedDate, centerX, centerY, isInAmbientMode);
         drawHorizontalPartition(canvas, centerX, centerY, isInAmbientMode);
-        drawWeatherInformation(canvas, centerX, centerY, high, low, isInAmbientMode, weatherIcon);
+        drawWeatherInformation(canvas, weatherViewModel, centerX, centerY, isInAmbientMode);
         onDrawListener.onDrawSuccess();
-    }
-
-    private void drawHorizontalPartition(Canvas canvas, float centerX, float centerY, boolean isInAmbientMode) {
-        if (isInAmbientMode) return;
-        float xCoordinateFactor = (centerX / 5) * 2;
-        float startPoint = xCoordinateFactor * 2;
-        float endPoint = xCoordinateFactor * 3;
-        canvas.drawLine(startPoint, centerY + 20, endPoint, centerY + 20, textPaint());
-    }
-
-    private void drawWeatherInformation(Canvas canvas, float centerX, float centerY, Double high,
-                                        Double low, boolean isInAmbientMode, Bitmap weatherIcon) {
-
-        if (isInAmbientMode) return;
-        if (Double.isNaN(high) && Double.isNaN(low)) return;
-        String highTemperature = high.toString().substring(0, 2) + DEGREE;
-        String lowTemperature = low.toString().substring(0, 2) + DEGREE;
-        drawText(canvas, highTemperature, centerX + 5, bigTextPaint(40), centerY + 80);
-        drawText(canvas, lowTemperature, centerX + 70, lightPaintWithSlimText(40), centerY + 80);
-        if (isWeatherIconPresent(weatherIcon))
-            canvas.drawBitmap(weatherIcon, centerX - 110, centerY + 30, textPaint());
-    }
-
-    private boolean isWeatherIconPresent(Bitmap weatherIcon) {
-        return weatherIcon != null;
-    }
-
-    private Paint lightPaintWithSlimText(int textSize) {
-        Typeface typeface = Typeface.create("sans-serif-light", Typeface.NORMAL);
-        Paint paint = lightPaint(textSize);
-        paint.setTypeface(typeface);
-        return paint;
-    }
-
-    @NonNull
-    private Paint lightPaint(int textSize) {
-        Paint paintLowTemperature = bigTextPaint(textSize);
-        paintLowTemperature.setColor(resources.getColor(R.color.light_watchface_text));
-        return paintLowTemperature;
-    }
-
-    private Paint bigTextPaint(int textSize) {
-        Paint paint = new Paint();
-        paint.setColor(resources.getColor(R.color.white));
-        paint.setTextSize(textSize);
-        paint.setAntiAlias(true);
-        return paint;
-    }
-
-    @NonNull
-    private Paint textPaint() {
-        Paint paint = new Paint();
-        paint.setColor(resources.getColor(R.color.white));
-        paint.setAntiAlias(true);
-        return paint;
     }
 
     private void setBackground(Canvas canvas, boolean isInAmbientMode) {
@@ -112,21 +73,99 @@ public class WatchFace {
         canvas.drawPaint(backgroundPaint);
     }
 
-    private void drawTime(Canvas canvas, String formattedTime, float centerX, float centerY, Paint paint) {
-        drawText(canvas, formattedTime, centerX, paint, centerY - 50);
+    private void drawHorizontalPartition(Canvas canvas, float centerX, float centerY, boolean isInAmbientMode) {
+        if (isInAmbientMode) return;
+        float xCoordinateFactor = (centerX / 5) * 2;
+        float startPoint = xCoordinateFactor * 2;
+        float endPoint = xCoordinateFactor * 3;
+        canvas.drawLine(startPoint, centerY + 20, endPoint, centerY + 20, textPaint());
     }
 
-    private void drawDateBelowTime(Canvas canvas, String formattedDate, float centerX, float centerY, Paint paint, boolean isInAmbientMode) {
-        if (isInAmbientMode) paint.setColor(resources.getColor(R.color.white));
-        drawText(canvas, formattedDate, centerX, paint, centerY - 5);
+    private void drawWeatherInformation(Canvas canvas, WeatherViewModel weatherViewModel,
+                                        float centerX, float centerY, boolean isInAmbientMode) {
+
+        if (isInAmbientMode || weatherViewModel.shouldNotDisplay()) return;
+        String highTemperature = weatherViewModel.highTemperature();
+        String lowTemperature = weatherViewModel.lowTemperature();
+        drawHighTemperature(canvas, centerX, centerY, highTemperature);
+        drawLowTemperature(canvas, centerX, centerY, lowTemperature);
+        drawWeatherIcon(canvas, weatherViewModel, centerX, centerY);
+    }
+
+    private void drawWeatherIcon(Canvas canvas, WeatherViewModel weatherViewModel, float centerX, float centerY) {
+        if (weatherViewModel.isWeatherIconPresent())
+            canvas.drawBitmap(weatherViewModel.weatherIcon, centerX - OFFSET_X_WEATHER_ICON,
+                    centerY + OFFSET_Y_WEATHER_ICON, textPaint());
+    }
+
+    private void drawLowTemperature(Canvas canvas, float centerX, float centerY, String lowTemperature) {
+        drawText(canvas, lowTemperature, centerX + OFFSET_X_LOW_TEMPERATURE,
+                lightPaintWithBigSlimText(TEXT_SIZE_LOW_TEMPERATURE, false),
+                centerY + OFFSET_Y_LOW_TEMPERATURE);
+    }
+
+    private void drawHighTemperature(Canvas canvas, float centerX, float centerY, String highTemperature) {
+        drawText(canvas, highTemperature, centerX + OFFSET_X_HIGH_TEMPERATURE,
+                bigTextPaint(TEXT_SIZE_HIGH_TEMPERATURE), centerY + OFFSET_Y_HIGH_TEMPERATURE);
+    }
+
+    private Paint lightPaintWithBigSlimText(int textSize, boolean isInAmbientMode) {
+        Typeface typeface = Typeface.create(TYPEFACE_SANS_SERIF_LIGHT, Typeface.NORMAL);
+        Paint paint = lightPaintWithBigText(textSize, isInAmbientMode);
+        paint.setTypeface(typeface);
+        return paint;
+    }
+
+    @NonNull
+    private Paint lightPaintWithBigText(int textSize, boolean isInAmbientMode) {
+        Paint lightPaint = bigTextPaint(textSize);
+        if (isInAmbientMode) lightPaint.setColor(resources.getColor(R.color.white));
+        else lightPaint.setColor(resources.getColor(R.color.light_watchface_text));
+        return lightPaint;
+    }
+
+    private Paint bigTextPaint(int textSize) {
+        Paint bigTextPaint = textPaint();
+        bigTextPaint.setTextSize(textSize);
+        return bigTextPaint;
+    }
+
+    private Paint bigAntiAliasTextPaint(int textSize) {
+        Paint bigTextPaint = bigTextPaint(textSize);
+        bigTextPaint.setAntiAlias(false);
+        return bigTextPaint;
+    }
+
+    @NonNull
+    private Paint textPaint() {
+        Paint paint = new Paint();
+        paint.setColor(resources.getColor(R.color.white));
+        paint.setAntiAlias(true);
+        return paint;
+    }
+
+    private void drawTime(Canvas canvas, String formattedTime, float centerX, float centerY, boolean isInAmbientMode) {
+        Paint paint = (isInAmbientMode) ? bigAntiAliasTextPaint(TEXT_SIZE_TIME) : bigTextPaint(TEXT_SIZE_TIME);
+        drawText(canvas, formattedTime, centerX, paint, centerY - OFFSET_Y_TIME);
+    }
+
+    private void drawDateBelowTime(Canvas canvas, String formattedDate, float centerX,
+                                   float centerY, boolean isInAmbientMode) {
+
+        Paint paint = lightPaintWithBigText(TEXT_SIZE_DATE, isInAmbientMode);
+        drawText(canvas, formattedDate, centerX, paint, centerY - OFFSET_Y_DATE);
     }
 
     private void drawText(Canvas canvas, @NonNull String text, float centerX, Paint paint, float positionY) {
-        float positionOnXCoordinate = centerX - (widthOfText(text, paint) / 2f);
+        float positionOnXCoordinate = centerX - halfOfWidthOfText(text, paint);
         canvas.drawText(text, positionOnXCoordinate, positionY, paint);
     }
 
+    private float halfOfWidthOfText(@NonNull String text, Paint paint) {
+        return widthOfText(text, paint) / 2f;
+    }
+
     private float widthOfText(String text, Paint paint) {
-        return paint.measureText(text, 0, text.length());
+        return paint.measureText(text, INDEX_TEXT_START, text.length());
     }
 }
